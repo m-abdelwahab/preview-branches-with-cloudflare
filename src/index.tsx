d8d69d86aad4a69f2e84f230b987054fa1ac767a
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-07-28 09:39:05 +0300

    chore: Add initial project files and dependencies

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+import { renderer } from "./renderer";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./lib/db/schema";
+
+type Bindings = {
+	DATABASE_URL: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(renderer);
+
+app.get("/", async (c) => {
+	const sql = neon(c.env.DATABASE_URL);
+
+	const db = drizzle(sql, {
+		schema,
+	});
+
+	const authors = await db.query.authors.findMany();
+
+	return c.json(authors);
+});
+
+export default app;
